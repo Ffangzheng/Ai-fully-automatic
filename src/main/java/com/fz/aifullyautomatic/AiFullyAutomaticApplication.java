0194ac627a3a2eb8e8364d81c56ec98fd1c72419
方正 <2292441626@qq.com>
2026-03-18 08:11:59 +0800

    INIT:创建原始项目

---
+++ src/main/java/com/fz/aifullyautomatic/AiFullyAutomaticApplication.java
@@ -0,0 +1,13 @@
+package com.fz.aifullyautomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFullyAutomaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFullyAutomaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fz/aifullyautomatic/AiFullyAutomaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fz.aifullyautomatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFullyAutomaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
